8d91bc2fbf9d29b718e23542456ce9c2f56e3120
Lefted <GartnerMoritz@yahoo.de>
2020-03-28 11:50:36 +0100

    Andere Spielfeld Skalierung

--- MuehleV2/src/me/moritz/Oberflaeche.java
+++ MuehleV2/src/me/moritz/Oberflaeche.java
@@ -16,8 +16,8 @@ import javax.swing.border.EmptyBorder;
 public class Oberflaeche extends JFrame {
 
     // KONSTANTEN
-    public static final int FRAME_WIDTH = 800;
-    public static final int FRAME_HEIGHT = 600;
+    public static final int PANE_WIDTH = 810;
+    public static final int PANE_HEIGHT = 810;
 
     // ATTRIBUTE
     private JPanel contentPane;
--- MuehleV2/src/me/moritz/Spielfeld.java
+++ MuehleV2/src/me/moritz/Spielfeld.java
@@ -1,41 +1,105 @@
 package me.moritz;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class Spielfeld extends JPanel {
 
     // ATTRIBUTE
-    Oberflaeche dieOberflaeche;
+    private Oberflaeche dieOberflaeche;
+    private Steuerung dieSteuerung;
+    private Knotenpunkt[] dieKnotenpunkte = new Knotenpunkt[24];
 
     // KONSTRUKTOR
     public Spielfeld(Oberflaeche dieOberflaeche) {
-	super();
 	this.dieOberflaeche = dieOberflaeche;
+	this.dieSteuerung = dieOberflaeche.getDieSteuerung();
+
+	// Double-Buffering aktivieren
+	setDoubleBuffered(true);
+
+	addMouseListener(new MouseAdapter() {
+	    @Override
+	    public void mouseClicked(MouseEvent e) {
+		// DEBUG
+		// System.out.println("x:" + e.getX() + " y:" + e.getY());
+
+		// DEBUG
+		// Knotenpunkte zeichnen
+		for (Knotenpunkt knotenpunkt : dieKnotenpunkte) {
+		    if (knotenpunkt != null) {
+			if (knotenpunkt.istMausÜber(e.getX(), e.getY())) {
+			    System.out.println("Knotenpunkt geklickt");
+			}
+		    }
+		}
+	    }
+	});
+
+	addMouseMotionListener(new MouseAdapter() {
+	    @Override
+	    public void mouseMoved(MouseEvent e) {
+		for (Knotenpunkt knotenpunkt : dieKnotenpunkte) {
+		    if (knotenpunkt != null) {
+			if (knotenpunkt.istMausÜber(e.getX(), e.getY())) {
+			    knotenpunkt.setFarbe(Color.BLUE);
+			} else {
+			    knotenpunkt.setFarbe(Color.RED);
+			}
+		    }
+		}
+	    }
+	});
+
+	// Knotenpunkte erstellen
+	dieKnotenpunkte[0] = new Knotenpunkt(150, 50);
+	dieKnotenpunkte[1] = new Knotenpunkt(230 - 12, 290 - 12);
     }
 
     // METHODEN
     @Override
     public Dimension getPreferredSize() {
-	return new Dimension(Oberflaeche.FRAME_WIDTH, Oberflaeche.FRAME_HEIGHT);
+	return new Dimension(Oberflaeche.PANE_WIDTH, Oberflaeche.PANE_HEIGHT);
     }
 
+    // zeichnen
     @Override
     protected void paintComponent(Graphics g) {
-	// Hintergrund zeichnen
+	// Hintergrund des Panels zeichnen
 	super.paintComponent(g);
 
-	g.clearRect(0, 0, Oberflaeche.FRAME_WIDTH, Oberflaeche.FRAME_HEIGHT);
+	// Alles vorher Gezeichnete löschen
+	g.clearRect(0, 0, Oberflaeche.PANE_WIDTH, Oberflaeche.PANE_HEIGHT);
 
-	g.setColor(Color.RED);
-	g.fillRect(this.dieOberflaeche.getRedX(), this.dieOberflaeche.getRedY(), 40, 40);
+	// Wenn im Menu nicht zeichnen
+	if (dieSteuerung.getSpielzustand() == Spielzustand.MENU) {
+	    return;
+	}
+
+	// Spielfeld Hintergrund zeichnen
+	
+	// für pane 800 600
+//	g.drawImage(Utilities.ladeBild("/spielfeld1200.png"), 140, 40, 520, 520, this);
+//	g.drawImage(Utilities.ladeBild("/spielfeld810.png"), 0, 0, this);
+	g.drawImage(Utilities.ladeBild("/spielfeld730.png"), 40, 40, this);
+	
+
+	// DEBUG
+	// Knotenpunkte zeichnen
+	for (Knotenpunkt knotenpunkt : dieKnotenpunkte) {
+	    if (knotenpunkt != null) {
+		knotenpunkt.zeichnen(g);
+	    }
+	}
     }
-
-    @Override
-    public boolean isDoubleBuffered() {
-	return true;
-    }
-
 }

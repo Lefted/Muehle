af059380ee7e386e54c1f4e0cfaedf002cf096f5
Lefted <gartnermoritz@yahoo.de>
2021-04-11 20:50:20 +0200

    Parse Arguments

---
+++ src/me/moritz/muehle/arguments/MultiplayerArguments.java
@@ -0,0 +1,11 @@
+package me.moritz.muehle.arguments;
+
+public class MultiplayerArguments {
+
+    public String ip = "127.0.0.1";
+    public int port = 1223;
+    public boolean server = false;
+    
+    public boolean hasFirstMove = true;
+    public boolean useWhite = true;
+}

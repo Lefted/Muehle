a79b7dbace5f967b9873ca43b1e9bc25cef47c6e
Lefted <gartnermoritz@yahoo.de>
2021-04-11 20:36:09 +0200

    Add PutPacket

---
+++ src/me/moritz/muehle/multiplayer/network/packets/PutPacket.java
@@ -0,0 +1,30 @@
+package me.moritz.muehle.multiplayer.network.packets;
+
+public class PutPacket extends Packet {
+
+    public static final int TYPE_ID = 2;
+
+    private final int row;
+    private final int column;
+    private final int radius;
+
+    public PutPacket(int row, int column, int radius) {
+	super(TYPE_ID);
+	
+	this.row = row;
+	this.column = column;
+	this.radius = radius;
+    }
+
+    public int getRow() {
+	return row;
+    }
+
+    public int getColumn() {
+	return column;
+    }
+
+    public int getRadius() {
+	return radius;
+    }
+}
